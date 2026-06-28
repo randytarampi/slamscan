c7584033a9778c7787bdd44190981024f963ff91
Randy Tarampi <rt@randytarampi.ca>
2026-06-28 15:58:04 +0200

    fix: swap portal deps to published version ranges. 🤝

--- src/serverless/aws.js
+++ src/serverless/aws.js
@@ -1,7 +1,6 @@
 import {configureAwsSdk} from "@randy.tarampi/serverless";
-import AWS from "aws-sdk";
 import logger from "./logger.js";
 
-export const Aws = configureAwsSdk(logger).captureAWS(AWS);
+export const Aws = configureAwsSdk(logger);
 
 export default Aws;
